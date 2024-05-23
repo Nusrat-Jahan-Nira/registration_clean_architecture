a8965b20e86c3c136b38d4d8ae53d3ab6c7f1f75
Nusrat Jahan <nusrat.jahan.nira@outlook.com>
2024-05-23 17:44:41 +0600

    validation function added

--- lib/src/feature/registration/domain/usecase/get_registration.dart
+++ lib/src/feature/registration/domain/usecase/get_registration.dart
@@ -1,3 +1,4 @@
+import 'package:registration_clean_architecture/src/core/util/validation/register_validation.dart';
 import 'package:registration_clean_architecture/src/feature/registration/data/model/Request/user_req_model.dart';
 import 'package:registration_clean_architecture/src/feature/registration/data/model/Response/user_res_model.dart';
 import 'package:registration_clean_architecture/src/feature/registration/domain/repository/user_repository.dart';
